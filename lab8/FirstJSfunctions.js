4dcdb36467067f40752d4ea49dd24c0f1344746f
ShuhanSun <sunshuhan666@gmail.com>
2020-11-15 19:03:55 -0800

    add ascii

--- lab8/FirstJSfunctions.js
+++ lab8/FirstJSfunctions.js
@@ -52,3 +52,24 @@ factory function instead of a function constructor in order to pass parameters s
 function f3(){
     let Teacher = {}
 }
+
+/*
+* Exercise 4:
+Write code that will create person, student, and professor objects.
+• Person objects have
+o name and age fields
+o a greeting method that prints out: “Greetings, my name is [name] and I am
+[age] years old.”
+o a salute method that prints out: “Good morning!, and in case I dont see you,
+good afternoon, good evening and good night!”
+• Student objects inherit name, age, and salute from person. They also have a field
+‘major’ and have their own greeting method. Their greeting is “Hey, my name is
+[name] and I am studying [major]. The greeting should be output to the console.
+• Professor objects inherit name, age, and salute from person. They also have a field
+‘department’ and have their own greeting method. Their salutation is “Good day,
+my name is [name] and I am in the [department] department.” Output it to the
+console.
+• Create a professor object and a student object. Call both the greeting and salutation
+methods on each.
+• Do this exercise once using the object prototype approach for inheritance and then
+using the function constructor approach.*/
