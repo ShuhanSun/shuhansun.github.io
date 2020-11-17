dbe35b16e032e2ad7df480079b3ef747f999b92b
ShuhanSun <sunshuhan666@gmail.com>
2020-11-17 00:26:47 -0800

    implement lab5 by map filter reduce

--- maplab5/FirstJSfunctions.js
+++ maplab5/FirstJSfunctions.js
@@ -1,79 +1,21 @@
-//     Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
-function max(a, b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;
-    }
-}
-
-//     Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
-function maxOfThree(a, b, c) {
-    return max(max(a, b), c);
-}
-
-//     Write a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-function isVowel(c) {
-    if (!c || c.length !== 1) {
-        return false;
-    }
-
-    let vowel = ['a', 'e', 'i', 'o', 'u']
-    for (const a of vowel) {
-        if (c === a) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 //     Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
 function sum(arr) {
-    let s = 0;
-    for (const a of arr) {
-        s += a;
-    }
-    return s;
+    return arr.reduce((prevValue, elem) => prevValue + elem);
 }
 
 function multiply(arr) {
-    let s = 1;
-    for (const a of arr) {
-        s *= a;
-    }
-    return s;
+    return arr.reduce((prevValue, elem) => prevValue * elem);
 }
 
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 function reverse(s) {
-    let r = '';
-    for (const c of s){
-        r = c + r;
-    }
-    return r;
+    return [...s].reduce((prevValue, elem) => elem + prevValue);
 }
 
-//     Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
-function findLongestWord(words) {
-    let maxLen = 0;
-    for (const w of words) {
-        if (maxLen < w.length) {
-            maxLen = w.length;
-        }
-    }
-    return maxLen;
-}
 
 //     Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
 function filterLongWords(words, i) {
-    let rArr = [];
-    for (const w of words) {
-        if (i < w.length) {
-            rArr.push(w);
-        }
-    }
-    return rArr;
+    return words.filter((w) => w.length > i);
 }
 
 //     Modify the jsfiddle on the map/filter/reduce slide as follows:
@@ -100,30 +42,18 @@ function myFunctionTest(expectation, func) {
     if (expectation === func()) {
         return "TEST SUCCEEDED";
     } else {
-        return "TEST FAILED. Expected " + expectation + " found " + func();;
+        return "TEST FAILED. Expected " + expectation + " found " + func();
+        ;
     }
 }
 
-console.log("Expected output of max(20,10) is 20 and  " +
-    myFunctionTest(20, function () {
-        return max(20, 10);
-    })
-);
-console.log("Expected output of maxOfThree(5,4,44) is 44  " + myFunctionTest(44, function () {
-    return maxOfThree(5, 4, 44);
+console.log("expected output of sum is 10 " + myFunctionTest(10, function () {
+    return sum([1, 2, 3, 4]);
 }));
-console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(55, function () {
-    return maxOfThree(55, 4, 44);
+console.log("expected output of sum is 24 " + myFunctionTest(24, function () {
+    return multiply([1, 2, 3, 4]);
 }));
-console.log("expected output of isVowel is true " + myFunctionTest(true, function() {return isVowel('i');}));
-console.log("expected output of isVowel is false " + myFunctionTest(false, function() {return isVowel('x');}));
 
-console.log("expected output of sum is 10 " + myFunctionTest(10, function() {return sum([1,2,3,4]);}));
-console.log("expected output of sum is 24 " + myFunctionTest(24, function() {return multiply([1,2,3,4]);}));
-
-console.log("expected output of reverse is gib " + myFunctionTest('gib', function() {return reverse('big');}));
-
-console.log("expected output of findLongestWord is 6 " + myFunctionTest(6, function() {return findLongestWord(['one','two','three','sixsix']);}));
 console.log("expected output of filterLongWords is 'apple' " +
     myFunctionTest('apple',
         function() {
@@ -136,5 +66,7 @@ console.log("expected output of filterLongWords is 'orange' " +
             const longWords = filterLongWords(['one', 'apple', 'orange', 'two', 'the'], 4);
             return longWords[1];
         }));
+
+
 
 stream();
