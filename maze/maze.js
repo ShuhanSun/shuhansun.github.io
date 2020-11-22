a4cbe89e79caf5345255f2e5d9d8775a9072939a
ShuhanSun <sunshuhan666@gmail.com>
2020-11-22 13:02:03 -0800

    finish maze

--- maze/maze.js
+++ maze/maze.js
@@ -1,5 +1,43 @@
-$(function() {
+/*global $ */
+
+$(function () {
     "use strict"
-    $('#start').click(function() { alert("Boink.");
+    const maze = $("#maze");
+    const start = $('#start');
+    const end = $('#end');
+    const boundary = $('.boundary');
+    const status = $('#status');
+    let hitWall = false;
+
+    const loss = function () {
+        if (!hitWall) {
+            boundary.addClass("youlose");
+            hitWall = true;
+        }
+    }
+    start.click(function () {
+        console.log("start");
+        // clear
+        hitWall = false;
+        if (boundary.hasClass("youlose")) {
+            boundary.removeClass("youlose");
+        }
+        status.text("Moving ... ");
+
+        // end
+        end.click(function () {
+                if (hitWall) {
+                    status.text("Sorry, you lost. :(");
+                } else {
+                    status.text("You win! :)");
+                }
+                //TODO remove boundary.mouseover
+            }
+        );
+
+        // loss
+        boundary.mouseover(loss);
+        maze.mouseleave(loss);
+
     });
-})
+});
